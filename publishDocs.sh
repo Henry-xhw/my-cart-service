dd05e41c89175c9131c468a16a6e15f427229403
Bryan Li <bryan.li@activenetwork.com>
2019-08-15 17:26:56 +0800

    DEV-DRIVEN Corrected the doc path

--- publishDocs.sh
+++ publishDocs.sh
@@ -3,7 +3,7 @@
 CURRENT_VERSION=$(sed -n 2p ./version.properties | sed -E 's/[a-zA-Z]*=([0-9.]*).*/\1/')
 LOCAL_DOC_PATH='build/asciidoc/html5'
 REMOTE_SERVER='sharedoc@share-apidocs.activenetwork.com'
-REMOTE_DOC_PATH='/opt/active/docs/fbi/cart-service'
+REMOTE_DOC_PATH='/opt/active/docs/oms/cart-service'
 
 echo 'Making dir if not exist'
 echo "Current version is ${CURRENT_VERSION}"
@@ -13,7 +13,7 @@ echo 'Making dir done'
 echo 'Publishing API doc'
 uploadDocCmd="scp -r ${LOCAL_DOC_PATH}/* ${REMOTE_SERVER}:${REMOTE_DOC_PATH}/${CURRENT_VERSION}"
 #upload the files to doc server:path
-#e.g. rsync -avz -e ssh build/asciidoc/html5/* jfang1@share-apidocs.activenetwork.com:/opt/active/docs/fbi/cart-service/0.0.1-SNAPSHOT
+#e.g. rsync -avz -e ssh build/asciidoc/html5/* sharedoc@share-apidocs.activenetwork.com:/opt/active/docs/oms/cart-service/0.0.1-SNAPSHOT
 ${uploadDocCmd}
 
 #change the remote latest.html to load the index.html in the latest version folder
