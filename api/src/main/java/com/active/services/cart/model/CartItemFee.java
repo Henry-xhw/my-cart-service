ee967db94cfaa9a6dbc80aee20c702e1fdfef69b
Jeff Fang <jeff.fang@activenetwork.com>
2019-09-04 17:24:03 -0700

    field rename in cart item fee

--- api/src/main/java/com/active/services/cart/model/CartItemFee.java
+++ api/src/main/java/com/active/services/cart/model/CartItemFee.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 public class CartItemFee {
     private String name;
     private BigDecimal unitPrice;
-    private int units;
-    private BigDecimal amount;
+    private Integer units;
+    private BigDecimal subtotal;
 }
