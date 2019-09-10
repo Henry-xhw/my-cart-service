7f937108a22cc786f4df8d6e807ec830383a745e
HenryXu <Henry_xhw45@163.com>
2019-09-10 15:00:32 +0800

    Add the CartItemFeeOrigin

---
+++ api/src/main/java/com/active/services/cart/model/CartItemFeeOrigin.java
@@ -0,0 +1,21 @@
+package com.active.services.cart.model;
+
+public enum CartItemFeeOrigin {
+    ACL,
+
+    TIMING,
+
+    TIMING_CHIP,
+
+    TIMER,
+
+    VEB_PLAN,
+
+    VEB_PLAN_ADD_ON,
+
+    TAX_CONSUMER_ABSORBED,
+
+    TAX_AGENCY_ABSORBED,
+
+    TAX_ACTIVE_PRODUCT
+}
