57858e532b10f232f7bf22202e187c9660e45afc
Jeff Fang <jeff.fang@activenetwork.com>
2019-11-07 15:02:34 -0800

    start to work on coupon

---
+++ src/main/java/com/active/services/cart/application/impl/CouponDiscountEngine.java
@@ -0,0 +1,15 @@
+package com.active.services.cart.application.impl;
+
+import com.active.services.cart.domain.cart.Cart;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponDiscountEngine {
+
+    public void apply(Cart cart, String coupon) {
+
+    }
+}
