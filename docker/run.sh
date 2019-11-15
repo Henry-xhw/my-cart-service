db66460e301f34c50f3c23064fd20cd7f1709902
hchen <herbert.chen@activenetwork.com>
2019-11-15 13:47:14 +0800

    Docker support

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -d --mount type=volume,destination=/var/opt/mssql --name ${2} --hostname ${1}-mssql -p ${3:-1434}:1434 ${1}
+docker run -d --mount type=volume,destination=/var/opt/mssql --name cart_service --hostname cart_service-mssql -p ${3:-1434}:1434 cart_service
