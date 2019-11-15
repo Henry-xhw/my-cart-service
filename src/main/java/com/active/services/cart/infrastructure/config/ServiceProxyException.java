b02b4bc93f7949d01b685267730e8100abc72eaa
hchen <herbert.chen@activenetwork.com>
2019-11-15 15:47:25 +0800

    Remove not used code

--- src/main/java/com/active/services/cart/infrastructure/config/ServiceProxyException.java
+++
@@ -1,7 +0,0 @@
-package com.active.services.cart.infrastructure.config;
-
-public class ServiceProxyException extends RuntimeException {
-    public ServiceProxyException(Exception e) {
-        super(e);
-    }
-}
--- src/main/java/com/active/services/cart/repository/mapper/CartMapper.java
+++ src/main/java/com/active/services/cart/repository/mapper/CartMapper.java
@@ -2,8 +2,8 @@ package com.active.services.cart.repository.mapper;
 
 import com.active.services.cart.domain.Cart;
 import com.active.services.cart.domain.CartItem;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.mapstruct.Mapper;
 
 import java.util.List;
 import java.util.UUID;
