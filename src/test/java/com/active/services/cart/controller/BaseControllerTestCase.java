a841f84c99c8cadb571e186d018fc771b79f79b4
wxu <wade.xu@activenetwork.com>
2019-11-27 14:49:48 +0800

    optimize restdocs

---
+++ src/test/java/com/active/services/cart/controller/BaseControllerTestCase.java
@@ -0,0 +1,27 @@
+package com.active.services.cart.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureRestDocs
+public class BaseControllerTestCase {
+
+    protected static final String ACTOR_ID = "Actor-Id";
+
+    protected static final String ACTOR_EMAIL = "product.service.ut@activenetwork.com";
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected HttpHeaders actorIdHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(ACTOR_ID, ACTOR_EMAIL);
+        return httpHeaders;
+    }
+}
