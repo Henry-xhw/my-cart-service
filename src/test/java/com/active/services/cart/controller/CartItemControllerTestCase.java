6c449b5148ec1db0b4e2bbad10dd204f94d95eaa
vpan1 <vick.pan@activenwork.com>
2019-11-28 17:30:51 +0800

    OMS-9243 Able to delete the cart item from cart                           add test and refactor code

--- src/test/java/com/active/services/cart/controller/CartItemControllerTestCase.java
+++ src/test/java/com/active/services/cart/controller/CartItemControllerTestCase.java
@@ -1,26 +1,40 @@
 package com.active.services.cart.controller;
 
-import com.active.services.cart.common.OperationResultCode;
-import com.active.services.cart.common.exception.CartException;
+
 import com.active.services.cart.controller.v1.CartItemController;
 import com.active.services.cart.domain.Cart;
 import com.active.services.cart.domain.CartItem;
-import com.active.services.cart.model.v1.rsp.DeleteCartItemRsp;
+import com.active.services.cart.model.v1.CartDto;
+import com.active.services.cart.model.v1.CartItemDto;
 import com.active.services.cart.service.CartService;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.*;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.UUID;
 
-@RunWith(MockitoJUnitRunner.class)
-public class CartItemControllerTestCase {
-    @Mock
+import static com.active.services.cart.controller.v1.Constants.V1_MEDIA;
+import static com.active.services.cart.restdocs.RestDocument.autoPathParameterDoc;
+import static com.active.services.cart.restdocs.RestDocument.newSuccessDocument;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static com.active.services.cart.restdocs.RestDocument.newErrorDocument;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = CartItemController.class, secure = false)
+public class CartItemControllerTestCase extends BaseControllerTestCase {
+
+    private static final String CONTENT_TYPE = "application/vnd.active.cart-service.v1+json";
+
+    @MockBean
     private CartService cartService;
-    @InjectMocks
-    private CartItemController itemController;
     private Cart cart;
     private UUID cartId = UUID.fromString("550e8400-e29b-41d4-a716-446655440000");
     private UUID cartItemId1 = UUID.fromString("550e8400-e29b-41d4-a716-446655440001");
@@ -34,48 +48,41 @@ public class CartItemControllerTestCase {
         CartItem cartItem1 = new CartItem();
         cartItem1.setIdentifier(cartItemId1);
         cart.getItems().add(cartItem1);
-        CartItem cartItem2 = new CartItem();
-        cartItem2.setIdentifier(cartItemId2);
-        cart.getItems().add(cartItem2);
-    }
-
-
-    @Test
-    public void delete_Common() throws Exception {
-        Mockito.when(cartService.get(cartId)).thenReturn(cart);
-        DeleteCartItemRsp rsp = itemController.delete(cartId, cartItemId1);
-        assert (rsp.getCartId().equals(cartId));
     }
 
     @Test
-    public void delete_cartNotExist() throws Exception {
-        Mockito.when(cartService.get(cartId)).thenReturn(null);
-        try {
-            itemController.delete(cartId, cartItemId1);
-        } catch (CartException exception) {
-            assert (exception.getErrorCode() == OperationResultCode.CART_NOT_EXIST.getCode());
-        }
+    public void deleteCartItemSuccess() throws Exception {
+        when(cartService.get(any(UUID.class))).thenReturn(cart);
+        mockMvc.perform(delete("/carts/{cart-id}/items/{cart-item-id}",
+                cartId, cartItemId1)
+                .headers(actorIdHeader())
+                .contentType(V1_MEDIA))
+                .andExpect(status().isOk())
+                .andDo(newSuccessDocument("Cart", "Delete-CartItem",
+                        pathParameters(autoPathParameterDoc("cart-id", CartDto.class, "identifier"),
+                                autoPathParameterDoc("cart-item-id", CartItemDto.class, "identifier"))));
     }
 
     @Test
-    public void delete_cartItemNotExist() throws Exception {
-        Mockito.when(cartService.get(cartId)).thenReturn(cart);
-        try {
-            itemController.delete(cartId, UUID.fromString("550e8400-e29b-41d4-a716-446655440003"));
-        } catch (CartException exception) {
-            assert (exception.getErrorCode() == OperationResultCode.CART_ITEM_NOT_EXIST.getCode());
-        }
+    public void deleteCartItemFailWhenCartNotExist() throws Exception {
+        when(cartService.get(any(UUID.class))).thenReturn(null);
+        mockMvc.perform(delete("/carts/{cart-id}/items/{cart-item-id}",
+                cartId, cartItemId1)
+                .headers(actorIdHeader())
+                .contentType(V1_MEDIA))
+                .andExpect(status().isOk())
+                .andDo(newErrorDocument("Cart", "Delete-CartItem", "Cart-Not-Exist"));
     }
 
     @Test
-    public void delete_cartItemNotExist_emptyCart() throws Exception {
-        cart.getItems().clear();
-        Mockito.when(cartService.get(cartId)).thenReturn(cart);
-        try {
-            itemController.delete(cartId, cartItemId1);
-        } catch (CartException exception) {
-            assert (exception.getErrorCode() == OperationResultCode.CART_ITEM_NOT_EXIST.getCode());
-        }
+    public void deleteCartItemFailWhenCartItemNotExist() throws Exception {
+        when(cartService.get(any(UUID.class))).thenReturn(cart);
+        mockMvc.perform(delete("/carts/{cart-id}/items/{cart-item-id}",
+                cartId, cartItemId2)
+                .headers(actorIdHeader())
+                .contentType(V1_MEDIA))
+                .andExpect(status().isOk())
+                .andDo(newErrorDocument("Cart", "Delete-CartItem", "CartItem-Not-Exist"));
     }
 
 }
