519b89c926c559914ff5a33831325a1a3687bc9b
Li, Terence <Terence.li@activenetwork.com>
2019-11-28 14:25:02 +0800

    OMS-9242 modify cartItemDto

--- api/src/main/java/com/active/services/cart/model/v1/CartItemDto.java
+++ api/src/main/java/com/active/services/cart/model/v1/CartItemDto.java
@@ -8,6 +8,9 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import javax.validation.Valid;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
@@ -23,8 +26,10 @@ public class CartItemDto extends BaseDto {
 
     private String productDescription;
 
+    @Valid
     private Range<Instant> bookingRange;
 
+    @Valid
     private Range<Instant> trimmedBookingRange;
 
     @NotNull
