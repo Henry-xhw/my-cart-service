7e6d91b1553284d3d985987efc0e73b37f9c17f2
HenryXu <Henry_xhw45@163.com>
2019-11-28 16:30:14 +0800

    OMS-9240 Add the unit test

--- src/test/java/com/active/services/cart/controller/CartControllerTestCase.java
+++ src/test/java/com/active/services/cart/controller/CartControllerTestCase.java
@@ -1,11 +1,20 @@
 package com.active.services.cart.controller;
 
-import com.active.services.cart.controller.v1.CartController;
-import com.active.services.cart.domain.Cart;
-import com.active.services.cart.mock.MockCart;
-import com.active.services.cart.model.v1.CartDto;
-import com.active.services.cart.model.v1.rsp.FindCartByIdRsp;
-import com.active.services.cart.service.CartService;
+import static com.active.services.cart.restdocs.RestDocument.autoApiDescriptionDoc;
+import static com.active.services.cart.restdocs.RestDocument.autoPathParameterDoc;
+import static com.active.services.cart.restdocs.RestDocument.autoRequestFieldsDoc;
+import static com.active.services.cart.restdocs.RestDocument.autoResponseFieldsDoc;
+import static com.active.services.cart.restdocs.RestDocument.newSuccessDocument;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.UUID;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,15 +23,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.UUID;
-
-import static com.active.services.cart.restdocs.RestDocument.autoPathParameterDoc;
-import static com.active.services.cart.restdocs.RestDocument.autoResponseFieldsDoc;
-import static com.active.services.cart.restdocs.RestDocument.newSuccessDocument;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.active.services.cart.controller.v1.CartController;
+import com.active.services.cart.domain.Cart;
+import com.active.services.cart.mock.MockCart;
+import com.active.services.cart.model.v1.CartDto;
+import com.active.services.cart.model.v1.req.CreateCartReq;
+import com.active.services.cart.model.v1.rsp.FindCartByIdRsp;
+import com.active.services.cart.service.CartService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = CartController.class, secure = false)
@@ -51,5 +58,25 @@ public class CartControllerTestCase extends BaseControllerTestCase {
                 .andReturn().getResponse().getContentAsString();
         CartDto cartDto = objectMapper.readValue(result, FindCartByIdRsp.class).getCart();
         Assert.assertNotNull(cartDto);
+    }
+
+    @Test public void createCartTest() throws Exception {
+        CreateCartReq req = new CreateCartReq();
+        req.setCart(MockCart.mockCartDto());
+        CreateCartReq rsp = new CreateCartReq();
+        CartDto cartDto = MockCart.mockCartDto();
+        cartDto.getItems().clear();
+        rsp.setCart(cartDto);
+        doNothing().when(cartService).create(any(Cart.class));
+        String result = mockMvc.perform(
+            post("/carts").contentType(CONTENT_TYPE).accept(CONTENT_TYPE).headers(actorIdHeader())
+                .content(objectMapper.writeValueAsString(req))).andExpect(status().isOk()).andDo(
+            newSuccessDocument("Cart", "Create-Cart", autoApiDescriptionDoc(CartController.class, "create"),
+                autoRequestFieldsDoc(req), autoResponseFieldsDoc(rsp)))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.cart.currencyCode").value("USD")).andReturn().getResponse()
+            .getContentAsString();
+        CartDto resultDto = objectMapper.readValue(result, CreateCartReq.class).getCart();
+        Assert.assertNotNull(resultDto);
+        Assert.assertFalse(cartDto.getIdentifier().toString().equals(resultDto.getIdentifier().toString()));
     }
 }
