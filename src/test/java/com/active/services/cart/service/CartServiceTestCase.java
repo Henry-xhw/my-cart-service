45150308513a340856bd7209dd242e1e044dc2ce
evanwei <Evan.wei@activenetwork.com>
2019-12-17 17:09:16 +0800

    add UT

--- src/test/java/com/active/services/cart/service/CartServiceTestCase.java
+++ src/test/java/com/active/services/cart/service/CartServiceTestCase.java
@@ -3,20 +3,28 @@ package com.active.services.cart.service;
 import com.active.services.cart.common.exception.CartException;
 import com.active.services.cart.domain.Cart;
 import com.active.services.cart.domain.CartDataFactory;
+import com.active.services.cart.domain.CartItem;
+import com.active.services.cart.domain.CartItemFee;
 import com.active.services.cart.repository.CartRepository;
+import com.active.services.cart.service.quote.CartPriceEngine;
+import com.active.services.cart.util.DataAccess;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -24,6 +32,12 @@ public class CartServiceTestCase {
 
     @Mock
     private CartRepository cartRepository;
+
+    @Mock
+    private CartPriceEngine cartPriceEngine;
+
+    @Mock
+    private DataAccess dataAccess;
 
     @InjectMocks
     private CartService cartService;
@@ -91,5 +105,19 @@ public class CartServiceTestCase {
         Cart cart = CartDataFactory.cart();
         cartService.create(cart);
         Mockito.verify(cartRepository).createCart(cart);
+    }
+
+    @Test
+    public void quoteSuccess() {
+        UUID cartId = UUID.fromString("BA5ED9E7-A2F2-F24B-CDA4-6399D76F0D4D");
+        Cart cart = CartDataFactory.cart();
+        CartItem cartItem = CartDataFactory.cartItem();
+        cartItem.setId(1L);
+        CartItemFee cartItemFee = CartDataFactory.cartItemFee();
+        cartItem.setFees(Arrays.asList(cartItemFee));
+        cart.setItems(Arrays.asList(cartItem));
+        when(cartRepository.getCart(cartId)).thenReturn(Optional.ofNullable(cart));
+        doNothing().when(cartPriceEngine).quote(any());
+        cartService.quote(cartId);
     }
 }
