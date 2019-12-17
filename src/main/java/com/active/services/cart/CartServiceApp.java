17754069740c812e4e103833431682a87124f959
hchen <herbert.chen@activenetwork.com>
2019-12-17 09:59:03 +0800

    Fix compile error

--- src/main/java/com/active/services/cart/CartServiceApp.java
+++ src/main/java/com/active/services/cart/CartServiceApp.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
-@EnableAspectJAutoProxy
 public class CartServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(CartServiceApp.class, args);
