cefe05a01383b6540936ae4f8738cfccb0fee2f1
vpan1 <vick.pan@activenwork.com>
2019-12-25 13:58:53 +0800

    fix code review

--- src/main/java/com/active/services/cart/service/quote/CartUnitPricePricer.java
+++ src/main/java/com/active/services/cart/service/quote/CartUnitPricePricer.java
@@ -1,18 +1,26 @@
 package com.active.services.cart.service.quote;
 
+import com.active.services.cart.domain.CartItem;
+import com.active.services.cart.util.TreeBuilder;
+
 import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CartUnitPricePricer implements CartPricer {
     @Override
     public void quote(CartQuoteContext context) {
-        context.getCart().getItems().forEach(cartItem -> {
+        List<CartItem> flattenCartItems = context.getCart().getFlattenCartItems();
+        flattenCartItems.forEach(cartItem -> {
             getCartItemPricer().quote(context, cartItem);
         });
+        TreeBuilder<CartItem> baseTreeTreeBuilder = new TreeBuilder<>(flattenCartItems);
+        context.getCart().setItems(baseTreeTreeBuilder.buildTree());
     }
 
     @Lookup
