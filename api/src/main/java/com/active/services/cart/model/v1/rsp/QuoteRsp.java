1d917f6f922ebaee11d6aa1cdd2deff85c35bf52
evanwei <Evan.wei@activenetwork.com>
2019-12-26 10:43:58 +0800

    QuoteRsp and CheckoutRsp extends BaseRsp

--- api/src/main/java/com/active/services/cart/model/v1/rsp/QuoteRsp.java
+++ api/src/main/java/com/active/services/cart/model/v1/rsp/QuoteRsp.java
@@ -4,6 +4,6 @@ import com.active.services.cart.model.v1.QuoteCartDto;
 import lombok.Data;
 
 @Data
-public class QuoteRsp {
+public class QuoteRsp extends BaseRsp {
     private QuoteCartDto cartDto;
 }
