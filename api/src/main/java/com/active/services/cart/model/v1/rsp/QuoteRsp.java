41c8b3b19057f331705f86dc382aee1b3f2fcfc2
Kim Tang <kimi.tang@activenetwork.com>
2019-12-26 13:57:29 +0800

    OMS-10110 compile error

--- api/src/main/java/com/active/services/cart/model/v1/rsp/QuoteRsp.java
+++ api/src/main/java/com/active/services/cart/model/v1/rsp/QuoteRsp.java
@@ -3,8 +3,10 @@ package com.active.services.cart.model.v1.rsp;
 import com.active.services.cart.model.v1.QuoteCartDto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class QuoteRsp extends BaseRsp {
     private QuoteCartDto cartDto;
 }
