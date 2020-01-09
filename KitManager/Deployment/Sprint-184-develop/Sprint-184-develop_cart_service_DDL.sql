6dd704acd250209839f271115a0a0afff9c3508f
Service Account - Kitmanager <svc.kitmanager@active.local>
2020-01-09 07:57:22 +0000

    DEV-DRIVEN Kit Refreshed by worker process (ACTIVE\svc.kitmanager)

--- KitManager/Deployment/Sprint-184-develop/Sprint-184-develop_cart_service_DDL.sql
+++ KitManager/Deployment/Sprint-184-develop/Sprint-184-develop_cart_service_DDL.sql
@@ -6,11 +6,43 @@ GO
 --FileName=delete.sql
 --SubmittedBy=evan wei (ACTIVE\ewei)
 
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables t WITH(NOLOCK)
+WHERE SCHEMA_NAME(schema_id) = 'dbo' AND OBJECT_NAME(object_id) ='cart' AND type = 'U')
+BEGIN
+	 drop table dbo.cart
+END
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables t WITH(NOLOCK)
+WHERE SCHEMA_NAME(schema_id) = 'dbo' AND OBJECT_NAME(object_id) ='cart_item' AND type = 'U')
+BEGIN
+	 drop table dbo.cart_item
+END
 
-drop table dbo.carts
-drop table dbo.cart_items
-drop table dbo.cart_item_fees
-drop table dbo.cart_item_cart_item_fees
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables t WITH(NOLOCK)
+WHERE SCHEMA_NAME(schema_id) = 'dbo' AND OBJECT_NAME(object_id) ='cart_items' AND type = 'U')
+BEGIN
+	 drop table dbo.cart_items
+END
+
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables t WITH(NOLOCK)
+WHERE SCHEMA_NAME(schema_id) = 'dbo' AND OBJECT_NAME(object_id) ='cart_item_cart_item_fees' AND type = 'U')
+BEGIN
+	 drop table dbo.cart_item_cart_item_fees
+END
+
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables t WITH(NOLOCK)
+WHERE SCHEMA_NAME(schema_id) = 'dbo' AND OBJECT_NAME(object_id) ='carts' AND type = 'U')
+BEGIN
+	 drop table dbo.carts
+END
+
+IF EXISTS(SELECT TOP 1 1 FROM sys.tables t WITH(NOLOCK)
+WHERE SCHEMA_NAME(schema_id) = 'dbo' AND OBJECT_NAME(object_id) ='cart_item_fees' AND type = 'U')
+BEGIN
+	 drop table dbo.cart_item_fees
+END
+
+
+
 GO
 --/KitManagerFileID=17922
 if exists(select top 1 1  from msdb.INFORMATION_SCHEMA.ROUTINES where routine_name='p_KitFileApplicationHistory_ins_Info')
