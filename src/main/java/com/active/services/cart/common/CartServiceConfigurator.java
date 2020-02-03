b519ffae7e86dc54e73cefe913c16193623c133a
hchen <herbert.chen@activenetwork.com>
2020-02-03 13:27:28 +0800

    OMS-10484 context wrapper for cart-service

--- src/main/java/com/active/services/cart/common/CartServiceConfigurator.java
+++ src/main/java/com/active/services/cart/common/CartServiceConfigurator.java
@@ -1,11 +1,9 @@
 package com.active.services.cart.common;
 
-import com.active.platform.filter.ContextFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.eventbus.EventBus;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,15 +18,5 @@ public class CartServiceConfigurator {
     @Bean
     public EventBus eventBus() {
         return new EventBus("cart-service-event-bus");
-    }
-
-    @Bean
-    public FilterRegistrationBean contextFilter() {
-        FilterRegistrationBean contextFilterReg = new FilterRegistrationBean();
-
-        contextFilterReg.setFilter(new ContextFilter());
-        contextFilterReg.addUrlPatterns("/carts/*");
-
-        return contextFilterReg;
     }
 }
