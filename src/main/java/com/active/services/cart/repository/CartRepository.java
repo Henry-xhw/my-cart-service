89d54ea5391fc18066d3d7f93ab0120972dc4b90
hchen <herbert.chen@activenetwork.com>
2020-02-03 16:03:31 +0800

    OMS-10484 context wrapper for cart-service

--- src/main/java/com/active/services/cart/repository/CartRepository.java
+++ src/main/java/com/active/services/cart/repository/CartRepository.java
@@ -86,8 +86,8 @@ public class CartRepository {
         return cartMapper.releaseLock(cartId, modifiedBy);
     }
 
-    public void updateCartReservationId(UUID cartId, UUID identifier) {
-        cartMapper.updateCartReservationGroupId(cartId, identifier, ContextWrapper.get().getActorId());
+    public void updateCartReservationId(UUID cartId, UUID reservationId) {
+        cartMapper.updateCartReservationGroupId(cartId, reservationId, ContextWrapper.get().getActorId());
     }
 
     public void createEvents(List<Event> events) {
