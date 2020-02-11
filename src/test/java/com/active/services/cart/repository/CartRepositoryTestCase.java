e663686bd15539155e287ab5d51da920da3422ad
vpan1 <vick.pan@activenetwork.com>
2020-02-11 12:05:30 +0800

    Vick-OMS-10523 add UT

--- src/test/java/com/active/services/cart/repository/CartRepositoryTestCase.java
+++ src/test/java/com/active/services/cart/repository/CartRepositoryTestCase.java
@@ -1,7 +1,10 @@
 package com.active.services.cart.repository;
 
+import com.active.services.cart.common.Event;
 import com.active.services.cart.domain.Cart;
 import com.active.services.cart.domain.CartDataFactory;
+import com.active.services.cart.domain.CartItem;
+import com.active.services.cart.domain.CartItemFee;
 import com.active.services.cart.repository.mapper.CartMapper;
 
 import org.junit.Assert;
@@ -12,6 +15,7 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -56,6 +60,39 @@ public class CartRepositoryTestCase {
     }
 
     @Test
+    public void cartItemCRUD() {
+        List<CartItem> items = CartDataFactory.cart().getItems();
+        cartRepository.createCartItems(1L, items);
+        verify(cartMapper, times(items.size())).createCartItem(any(), any());
+
+        CartItem item = CartDataFactory.cartItem();
+        item.setId(1L);
+        CartItemFee cartItemFee = CartDataFactory.cartItemFee();
+        item.setFees(Collections.singletonList(cartItemFee));
+        long cartItemId = cartRepository.createCartItem(1L, item);
+        Assert.assertEquals(1L, cartItemId);
+        verify(cartMapper, times(items.size()+1)).createCartItem(any(), any());
+
+        List<CartItem> updateItems = CartDataFactory.cart().getItems();
+        cartRepository.updateCartItems(updateItems);
+        verify(cartMapper, times(updateItems.size())).updateCartItem(any());
+
+        cartRepository.deleteCartItem(1L);
+        verify(cartMapper, times(1)).deleteCartItem(any());
+
+        List<UUID> deleteList = new ArrayList();
+        deleteList.add(UUID.randomUUID());
+        cartRepository.batchDeleteCartItems(deleteList);
+        verify(cartMapper, times(1)).batchDeleteCartItems(any());
+
+        when(cartMapper.getCartItemIdByCartItemUuid(any())).thenReturn(Optional.of(1L));
+        Optional<Long> itemId = cartRepository.getCartItemIdByCartItemUuid(UUID.randomUUID());
+        Assert.assertTrue(itemId.isPresent());
+        Assert.assertEquals(1,itemId.get().longValue());
+    }
+
+
+    @Test
     public void cartStatusOperation() {
         when(cartMapper.finalizeCart(any(), any())).thenReturn(1);
         int result = cartRepository.finalizeCart(UUID.randomUUID(), "system");
@@ -84,6 +121,19 @@ public class CartRepositoryTestCase {
         int rest = cartRepository.releaseLock(UUID.randomUUID(), "system");
         Assert.assertEquals(1, rest);
 
+    }
+
+    @Test
+    public void updateCartReservationIdSuccess() {
+
+    }
+
+    @Test
+    public void createEventsSuccess() {
+        List<Event> events = new ArrayList<>();
+        events.add(new Event());
+        cartRepository.createEvents(events);
+        verify(cartMapper, times(1)).createEvents(any());
     }
 
 }
