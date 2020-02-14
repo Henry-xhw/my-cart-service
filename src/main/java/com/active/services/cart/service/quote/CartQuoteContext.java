2ce7bace0fac2e77cf045e31e79113f96ef36c59
Kim Tang <kimi.tang@activenetwork.com>
2020-02-14 08:54:34 +0800

    OMS-10463 coupon discount logic

--- src/main/java/com/active/services/cart/service/quote/CartQuoteContext.java
+++ src/main/java/com/active/services/cart/service/quote/CartQuoteContext.java
@@ -1,16 +1,27 @@
 package com.active.services.cart.service.quote;
 
+import com.active.services.DiscountModel;
 import com.active.services.cart.domain.Cart;
 import com.active.services.product.Product;
 
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Data
 public class CartQuoteContext {
     private Cart cart;
     private List<Product> products;
+
+    private Map<Long, Product> productsMap;
+
+    public DiscountModel getDiscountModel(Long discountId) {
+        return Optional.ofNullable(productsMap.get(discountId))
+                .map(Product::getDiscountModel)
+                .orElse(DiscountModel.COMBINABLE_FLAT_FIRST);
+    }
 
     public CartQuoteContext(Cart cart) {
         this.cart = cart;
