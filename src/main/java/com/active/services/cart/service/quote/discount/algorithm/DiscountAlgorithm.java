afe45d213ab9fbb9c87b32b6aa20a6d9ddf0c72c
Kim Tang <kimi.tang@activenetwork.com>
2020-02-18 15:12:04 +0800

    OMS-10463 coupon discount logic

--- src/main/java/com/active/services/cart/service/quote/discount/algorithm/DiscountAlgorithm.java
+++ src/main/java/com/active/services/cart/service/quote/discount/algorithm/DiscountAlgorithm.java
@@ -2,10 +2,8 @@ package com.active.services.cart.service.quote.discount.algorithm;
 
 import com.active.services.cart.service.quote.discount.Discount;
 
-import java.math.BigDecimal;
-import java.util.Currency;
 import java.util.List;
 
 public interface DiscountAlgorithm {
-    List<Discount> apply(List<Discount> discounts, BigDecimal amountToDiscount, Currency currency);
+    List<Discount> apply(List<Discount> discounts);
 }
