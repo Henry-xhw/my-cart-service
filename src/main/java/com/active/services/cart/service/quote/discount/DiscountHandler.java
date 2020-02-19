0069bd45d8603aab0e1bd196f36d03d3b12adc54
Kim Tang <kimi.tang@activenetwork.com>
2020-02-19 18:02:51 +0800

    OMS-10463 coupon discount logic

--- src/main/java/com/active/services/cart/service/quote/discount/DiscountHandler.java
+++ src/main/java/com/active/services/cart/service/quote/discount/DiscountHandler.java
@@ -1,8 +1,10 @@
 package com.active.services.cart.service.quote.discount;
 
+import com.active.services.cart.service.quote.discount.algorithm.DiscountAlgorithm;
+
 import java.util.List;
 
 public interface DiscountHandler {
     List<Discount> loadDiscounts();
-    List<Discount> filterAndSort(List<Discount> discounts);
+    DiscountAlgorithm getDiscountAlgorithm();
 }
