0744001faf17cf19a085287bc50a954cf620f149
evanwei <Evan.wei@activenetwork.com>
2020-02-25 15:02:10 +0800

    create unique constraint

--- KitManager/Databases/cart_service/Tables/cart_item_fee.sql
+++ KitManager/Databases/cart_service/Tables/cart_item_fee.sql
@@ -11,7 +11,7 @@ BEGIN
         [transaction_type]          NVARCHAR(25)        NOT NULL,
         [units]                     BIGINT              NOT NULL,
         [unit_price]                DECIMAL(19, 2)      NOT NULL,
-        [cart_discount_id]          BIGINT              NULL,
+        [related_identifier]        BIGINT              NULL,
         [created_by]                NVARCHAR(255)       NOT NULL,
         [created_dt]                DATETIME            NOT NULL,
         [modified_by]               NVARCHAR(255)       NOT NULL,
