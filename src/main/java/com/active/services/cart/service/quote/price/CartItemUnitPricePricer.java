c09f690eb55f8cf7551b8499fd78af617b51317f
Kim Tang <kimi.tang@activenetwork.com>
2020-02-27 11:08:11 +0800

    OMS-10463 update grossPrice and netPrice logic

--- src/main/java/com/active/services/cart/service/quote/price/CartItemUnitPricePricer.java
+++ src/main/java/com/active/services/cart/service/quote/price/CartItemUnitPricePricer.java
@@ -1,25 +1,17 @@
 package com.active.services.cart.service.quote.price;
 
 import com.active.services.cart.domain.CartItem;
-import com.active.services.cart.domain.CartItemFee;
-import com.active.services.cart.model.CartItemFeeType;
 import com.active.services.cart.service.quote.CartItemFeeBuilder;
 import com.active.services.cart.service.quote.CartItemPricer;
 import com.active.services.cart.service.quote.CartQuoteContext;
 import com.active.services.product.nextgen.v1.dto.fee.FeeDto;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-
-import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
 
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -30,28 +22,18 @@ public class CartItemUnitPricePricer implements CartItemPricer {
 
     @Override
     public void quote(CartQuoteContext context, CartItem cartItem) {
-        if (Objects.isNull(cartItem.getOverridePrice())) {
-            // no override price in CartItem
-            cartItem.getFees().add(CartItemFeeBuilder.buildPriceItemFee(cartItem.getQuantity(),
-                    feeDtoMap.get(cartItem.getProductId())));
-        } else {
+
+        if (cartItem.getOverridePrice() != null) {
             // has override price in CartItem
+            cartItem.setGrossPrice(cartItem.getOverridePrice());
             cartItem.getFees().add(CartItemFeeBuilder.buildOverridePriceItemFee(cartItem));
+            return;
         }
-        setGrossAndNetPriceValue(cartItem);
-    }
 
-    private void setGrossAndNetPriceValue(CartItem cartItem) {
-        Optional.ofNullable(cartItem.getFees()).ifPresent(
-            fees -> fees.stream().filter(cartItemFee -> Objects.equals(cartItemFee.getType(), CartItemFeeType.PRICE))
-                .findAny().ifPresent(cartItemFee -> {
-                    cartItem.setGrossPrice(calculateGrossPrice(cartItem, cartItemFee));
-                })
-        );
-    }
+        FeeDto feeDto = feeDtoMap.get(cartItem.getProductId());
+        cartItem.setGrossPrice(feeDto.getAmount());
+        // no override price in CartItem
+        cartItem.getFees().add(CartItemFeeBuilder.buildPriceItemFee(cartItem.getQuantity(), feeDto));
 
-    private BigDecimal calculateGrossPrice(CartItem cartItem, @NonNull CartItemFee priceFee) {
-        return cartItem.getOverridePrice() != null ? cartItem.getOverridePrice() : priceFee.getUnitPrice();
     }
-
 }
