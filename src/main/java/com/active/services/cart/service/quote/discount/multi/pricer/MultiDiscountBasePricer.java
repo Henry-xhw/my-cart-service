c55e5f7926537701200e3e869e074a083267908f
hchen <herbert.chen@activenetwork.com>
2020-03-03 09:47:05 +0800

    OMS-10462 multi discount

--- src/main/java/com/active/services/cart/service/quote/discount/multi/pricer/MultiDiscountBasePricer.java
+++ src/main/java/com/active/services/cart/service/quote/discount/multi/pricer/MultiDiscountBasePricer.java
@@ -6,22 +6,18 @@ import com.active.services.cart.service.quote.CartQuoteContext;
 import com.active.services.cart.service.quote.discount.multi.CartItemMultiDiscountPricer;
 import com.active.services.cart.service.quote.discount.multi.loader.MultiDiscountCartItem;
 import com.active.services.product.discount.multi.DiscountTier;
-import com.active.services.product.discount.multi.MultiDiscountAlgorithm;
 import com.active.services.product.discount.multi.MultiDiscountThresholdSetting;
 import com.active.services.product.discount.multi.SortedTierListBuilder;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
-import static com.active.services.cart.service.quote.discount.multi.MultiDiscountUtil.itemsByPersonIdentifier;
+import static com.active.services.oms.BdUtil.comparesToZero;
 import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor
@@ -35,8 +31,15 @@ public abstract class MultiDiscountBasePricer implements MultiDiscountPricer {
 
     @Override
     public final void price() {
+        // All items does not have net price.
+        if (mdCartItem.getCartItems().stream().allMatch(item -> comparesToZero(item.getNetPrice()))) {
+            return;
+        }
+
         // Step1: build effective cart items by different algorithm.
-        List<CartItem> effectiveSortedMdCartItems = buildEffectiveSortedMdCartItems();
+        EffectivePersonBasedMdCartItemBuilder builder = new EffectivePersonBasedMdCartItemBuilder(mdCartItem);
+        builder.allProductsComparator(getAllProductComparator());
+        List<CartItem> effectiveSortedMdCartItems = builder.build();
 
         // Step2: apply tiers
         applyTiers(effectiveSortedMdCartItems);
@@ -71,52 +74,18 @@ public abstract class MultiDiscountBasePricer implements MultiDiscountPricer {
     protected abstract int countTiers(List<CartItem> effectiveSortedMdCartItems);
 
     /**
-     * Filter and sort cart items by different algorithm
-     *
-     * @return
-     */
-    private List<CartItem> buildEffectiveSortedMdCartItems() {
-        MultiDiscountAlgorithm algorithm = mdCartItem.getMultiDiscount().getAlgorithm();
-        switch (algorithm) {
-            case LEAST_EXPENSIVE:
-                Function<List<CartItem>, CartItem> cartItemSelector = items -> sortByTotalPrice(items, true).get(0);
-                return new EffectivePersonBasedMdCartItemBuilder(mdCartItem, cartItemSelector).build();
-            case MOST_EXPENSIVE:
-                cartItemSelector = items -> sortByTotalPrice(items, false).get(0);
-                return new EffectivePersonBasedMdCartItemBuilder(mdCartItem, cartItemSelector).build();
-            case ALL_PRODUCTS:
-                return buildAllProductsEffectiveSortedCartItems();
-            default:
-                throw new IllegalStateException("Not supported MultiDiscountAlgorithm " + algorithm);
-        }
-    }
-
-    /**
-     * Sort ALL_RPODUCTS algorithm
-     *
-     * @return
-     */
-    private List<CartItem> buildAllProductsEffectiveSortedCartItems() {
-        Map<String, List<CartItem>> itemsByIdentifier = itemsByPersonIdentifier(getMdCartItem().getCartItems());
-        List<List<CartItem>> cartItemss = new ArrayList<>(itemsByIdentifier.values());
-        Collections.sort(cartItemss, getAllProductComparator());
-
-        return cartItemss.stream().flatMap(List::stream).collect(toList());
-    }
-
-    /**
      * Comparator first by CartItem net amounts, then by db index
      *
      * @return
      */
     protected static Comparator<List<CartItem>> totalPriceThenDbIndex() {
         return Comparator.comparing(Cart::getNetAmounts) // by total price
-                .thenComparing(items -> { // by min db index
-                    List<Long> itemIds = items.stream().map(CartItem::getId).collect(toList());
-                    Collections.sort(itemIds);
+            .thenComparing(items -> { // by min db index
+                List<Long> itemIds = items.stream().map(CartItem::getId).collect(toList());
+                Collections.sort(itemIds);
 
-                    return itemIds.get(0);
-                });
+                return itemIds.get(0);
+            });
     }
 
     @Override
@@ -130,22 +99,4 @@ public abstract class MultiDiscountBasePricer implements MultiDiscountPricer {
      * @return
      */
     protected abstract Comparator<List<CartItem>> getAllProductComparator();
-
-    /**
-     * Sort by total price, then by db id.
-     *
-     * @param cartItems
-     * @return
-     */
-    private List<CartItem> sortByTotalPrice(List<CartItem> cartItems, boolean asc) {
-        Comparator<CartItem> comparator = Comparator.comparing(CartItem::getNetAmount);
-        if (!asc) {
-            comparator = comparator.reversed();
-        }
-        comparator = comparator.thenComparing(CartItem::getId);
-
-        Collections.sort(cartItems, comparator);
-
-        return cartItems;
-    }
 }
