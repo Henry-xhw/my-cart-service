20182d8fd88a50fd0cb4dccf8abf59ac5d95f064
hchen <herbert.chen@activenetwork.com>
2020-03-04 19:20:29 +0800

    OMS-10462 format code

--- src/main/java/com/active/services/cart/service/quote/discount/multi/pricer/MultiDiscountPricer.java
+++ src/main/java/com/active/services/cart/service/quote/discount/multi/pricer/MultiDiscountPricer.java
@@ -1,7 +1,5 @@
 package com.active.services.cart.service.quote.discount.multi.pricer;
 
-import com.active.services.cart.service.quote.CartQuoteContext;
-
 public interface MultiDiscountPricer {
     void price();
 }
