2afddb17014d7e72b5cf20dc01ceb375188a7103
Kim Tang <kimi.tang@activenetwork.com>
2020-03-12 14:02:50 +0800

    OMS-10781 add discount usages.

--- src/main/java/com/active/services/cart/service/quote/discount/CartItemDiscounts.java
+++ src/main/java/com/active/services/cart/service/quote/discount/CartItemDiscounts.java
@@ -1,12 +1,15 @@
 package com.active.services.cart.service.quote.discount;
 
 import com.active.services.cart.domain.CartItem;
+import com.active.services.cart.domain.Discount;
 
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -22,5 +25,17 @@ public class CartItemDiscounts {
 
     public BigDecimal getTotalNetPrice() {
         return cartItem.getNetPrice().multiply(BigDecimal.valueOf(cartItem.getQuantity()));
+    }
+
+    public List<Long> getAllDiscountIds() {
+        return CollectionUtils.emptyIfNull(discounts).stream().map(Discount::getDiscountId)
+                .distinct().collect(Collectors.toList());
+    }
+
+    public CartItemDiscounts filterDiscounts(List<Long> filterIds) {
+        discounts =
+                CollectionUtils.emptyIfNull(discounts).stream().filter(discountApplication ->
+                        filterIds.contains(discountApplication.getDiscountId())).collect(Collectors.toList());
+        return this;
     }
 }
