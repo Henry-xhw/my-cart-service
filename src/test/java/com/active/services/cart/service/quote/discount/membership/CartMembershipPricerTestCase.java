bd3af5318dec96218a3bbffa42b4bdf8deaad7b3
Xiong <xiaoming.xiong@activenetwork.com>
2020-03-18 16:59:38 +0800

    self testing and add unit testcases

--- src/test/java/com/active/services/cart/service/quote/discount/membership/CartMembershipPricerTestCase.java
+++ src/test/java/com/active/services/cart/service/quote/discount/membership/CartMembershipPricerTestCase.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyObject;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
